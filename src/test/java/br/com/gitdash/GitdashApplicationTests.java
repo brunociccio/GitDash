a403cadaf750af20919c9759ac8c1fed432af35d
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-10-09 09:08:04 -0300

    upload do gitdash da api epic task

---
+++ src/test/java/br/com/gitdash/GitdashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gitdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
