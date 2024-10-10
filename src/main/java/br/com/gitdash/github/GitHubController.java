5edd0a932c29bc45a3e72eeb6168c46b1eaf1a0f
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-10-10 08:57:51 -0300

    configurações do secutiry e criação do login.html com autenticação do github

--- src/main/java/br/com/gitdash/github/GitHubController.java
+++ src/main/java/br/com/gitdash/github/GitHubController.java
@@ -1,12 +1,14 @@
 package br.com.gitdash.github;
 
+import java.util.List;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-
-import java.util.List;
 
 @Controller
 public class GitHubController {
@@ -17,13 +19,19 @@ public class GitHubController {
         this.gitHubService = gitHubService;
     }
 
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
     @GetMapping("/")
-    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient) {
+    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient, @AuthenticationPrincipal OAuth2User user) {
 
         String tokenValue = authorizedClient.getAccessToken().getTokenValue();
         List<RepositoryInfo> repos = gitHubService.getUserRepositories(tokenValue);
 
         model.addAttribute("repos", repos);
+        model.addAttribute("user", user);
 
         return "user";
     }
